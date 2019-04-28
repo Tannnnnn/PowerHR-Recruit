bd2b6a60c1aa7361f6c4a021aebf71fb02da8ce8
Pongsatorn001 <Pongsatorn698@gmail.com>
2019-04-28 15:13:46 +0700

    creace register

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { withLayout } from '../hoc'
 import styled from 'styled-components'
-import { compose , withProps, withHandlers, withState } from 'recompose'
+import { compose , withProps, withHandlers, withState , lifecycle } from 'recompose'
 import { Container, Grid, Button, Form } from 'semantic-ui-react'
 import theme from '../theme/default'
+import axios from 'axios'
 import { 
     input2GrideGrideMG,
     input2Gride
@@ -65,19 +66,67 @@ const ButtonCancel = styled(Button)`
 `;
 
 const enhance = compose(
+    withState('all_users','setAllUsers'),
     withState('firstName','setFirstName'),
+    withState('lastName','setLastName'),
+    withState('email','setEmail'),
+    withState('password','setPassword'),
+    withState('confirmPassword','setConfirmPassword'),
+    withState('idCard','setIdCard'),
     withProps({
         pageTitle: 'Register'
     }),
     withLayout,
+    lifecycle({
+        async componentDidMount(){
+          const url = `http://localhost:4000/user`
+          const res = await axios.get(url)
+          let users = []
+          res.data.map( data => {
+            users.push(data.firstName , data.lastName , data.email , data.password , data.idCard)
+          })
+          this.props.setAllUsers(users) 
+        }
+    }),
     withHandlers({
         handleFirstName : props => () => event => {
-            
+            props.setFirstName(event.target.value)
+        },
+        handleLastName : props => () => event => {
+            props.setLastName(event.target.value)
+        },
+        handleEmail : props => () => event => {
+            props.setEmail(event.target.value)
+        },
+        handlePassword : props => () => event => {
+            props.setPassword(event.target.value)
+        },
+        handleConfirmPassword : props => () => event => {
+            props.setConfirmPassword(event.target.value)
+        },
+        handleIdCard : props => () => event => {
+            props.setIdCard(event.target.value)
+        },
+        handleSaveUser : props => () => event => {
+            const url = 'http://localhost:4000/user'
+            axios.post(url , {
+                firstName : props.firstName,
+                lastName : props.lastName,
+                email : props.email,
+                password : props.password,
+                idCard : props.idCard
+            })
+            .then( res => {
+                console.log(res)
+            })
+            .catch( err => {
+                console.log(err);
+            })
         }
     })
 )
  
-export default enhance(() => 
+export default enhance((props) => 
     <div>
         {CarouselCompane ('CUPCODE CO., LTD.')}
         <Container>
@@ -94,31 +143,31 @@ export default enhance(() =>
                             </MgGridLeft>
                         </Grid.Column>
                         <Grid.Column>
-                            {input2Gride('นามสกุล (ภาษาไทย) :', 'กรุณากรอกนามสกุล (ภาษาไทย)')}
+                            {input2Gride('นามสกุล (ภาษาไทย) :', 'กรุณากรอกนามสกุล (ภาษาไทย)' , props.handleLastName() , 'text' , props.lastName)}
                         </Grid.Column>
                     </Grid>
                     <Grid columns={2} padded='horizontally'>
                         <Grid.Column>
                             <MgGridLeft>
-                                {input2GrideGrideMG('อีเมล :', 'กรุณากรอกอีเมล')}
+                                {input2GrideGrideMG('อีเมล :', 'กรุณากรอกอีเมล' , props.handleEmail() , 'email' , props.email)}
                             </MgGridLeft>
                         </Grid.Column>
                         <Grid.Column>
-                            {input2Gride('เลขบัตรประจำตัวประชาชน :', 'กรุณากรอกเลขบัตรประจำตัวประชาชน')}
+                            {input2Gride('เลขบัตรประจำตัวประชาชน :', 'กรุณากรอกเลขบัตรประจำตัวประชาชน' , props.handleIdCard() , 'text' , props.idCard)}
                         </Grid.Column>
                     </Grid>
                     <Grid columns={2} padded='horizontally'>
                         <Grid.Column>
                             <MgGridLeft>
-                                {input2GrideGrideMG('รหัสผ่าน :', 'กรุณากรอกรหัสผ่าน')}
+                                {input2GrideGrideMG('รหัสผ่าน :', 'กรุณากรอกรหัสผ่าน' , props.handlePassword() , 'password' , props.password)}
                             </MgGridLeft>
                         </Grid.Column>
                         <Grid.Column>
-                            {input2Gride('ยืนยันรหัสผ่าน :', 'ยืนยันรหัสผ่าน')}
+                            {input2Gride('ยืนยันรหัสผ่าน :', 'ยืนยันรหัสผ่าน' , props.handleConfirmPassword() , 'password' , props.confirmPassword)}
                         </Grid.Column>
                     </Grid>
-                <ButtonRegister type='submit'>ยืนยันการสมัครสมาชิก</ButtonRegister>
-                <ButtonCancel>ยกเลิกการสมัคร</ButtonCancel>
+                <ButtonRegister type='submit' onClick={props.handleSaveUser()}>ยืนยันการสมัครสมาชิก</ButtonRegister>
+                <ButtonCancel href="javascript:history.back()">ยกเลิกการสมัคร</ButtonCancel>
                 <br/><br/>
             </Box>
         </Container>
