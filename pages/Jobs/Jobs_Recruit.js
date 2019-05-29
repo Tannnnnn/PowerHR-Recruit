0cbf03397b99b88bbe778e8b8b463a675f4bd1ab
tannnnn <kitpakorn@cupcodeteam.com>
2019-05-29 19:13:47 +0700

    edit height loading

--- pages/Jobs/Jobs_Recruit.js
+++ pages/Jobs/Jobs_Recruit.js
@@ -232,7 +232,7 @@ export default enhance((props) =>
     <ContainerHeader>
         <SegmentHeader>ตำแหน่งงานที่เปิดรับสมัคร :</SegmentHeader>
         <ContainerContent>
-          <Loader size='medium' active={props.isLoading} style={{ top : "83%" }}>กำลังโหลดข้อมูล กรุณารอสักครู่...</Loader>
+          <Loader size='medium' active={props.isLoading} style={{ top : "73%" }}>กำลังโหลดข้อมูล กรุณารอสักครู่...</Loader>
           { props.recruit && props.handleShowData(props.handleSetDateInThai)}
         </ContainerContent>
     </ContainerHeader>
